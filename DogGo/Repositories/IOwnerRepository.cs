cd9f9382c5a73f57be7a17640dfa9c4bb54ca80d
Emily <emily.reed0913@gmail.com>
2021-12-13 15:07:31 -0600

    Practice exercises completed

---
+++ DogGo/Repositories/IOwnerRepository.cs
@@ -0,0 +1,15 @@
+﻿using DogGo.Models;
+using System;
+using System.Collections.Generic;
+using Microsoft.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DogGo.Repositories
+{
+        public interface IOwnerRepository
+        {
+        List<Owner> GetAllOwners();
+            Owner GetOwnerById(int id);
+        }
+}
---
+++ DogGo/Repositories/OwnerRepository.cs
@@ -0,0 +1,109 @@
+﻿using DogGo.Models;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System.Collections.Generic;
+
+namespace DogGo.Repositories
+{
+    public class OwnerRepository : IOwnerRepository
+    {
+        private readonly IConfiguration _config;
+
+        // The constructor accepts an IConfiguration object as a parameter. This class comes from the ASP.NET framework and is useful for retrieving things out of the appsettings.json file like connection strings.
+        public OwnerRepository(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public SqlConnection Connection
+        {
+            get
+            {
+                return new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            }
+        }
+
+        public List<Owner> GetAllOwners()
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                       SELECT o.Id, o.Email, o.[Name], o.Address,  n.Name as neighborhoodName, o.Phone
+                        FROM Owner o
+                        Join Neighborhood n on n.id=o.NeighborhoodId
+                    ";
+
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        List<Owner> owners = new List<Owner>();
+                        while (reader.Read())
+                        {
+                            Owner owner = new Owner
+                            {
+                                Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                                Email = reader.GetString(reader.GetOrdinal("Email")),
+                                Name = reader.GetString(reader.GetOrdinal("Name")),
+                                Address = reader.GetString(reader.GetOrdinal("Address")),
+                                Neighborhood = new Neighborhood
+                                {
+                                    Name = reader.GetString(reader.GetOrdinal("NeighborhoodName"))
+                                },
+                                Phone = reader.GetString(reader.GetOrdinal("Phone")),
+                            };
+
+                            owners.Add(owner);
+                        }
+
+                        return owners;
+                    }
+                }
+            }
+        }
+
+        public Owner GetOwnerById(int id)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                        SELECT o.Id as ownerId, o.Email, o.[Name], o.Address, n.Name as neighborhoodName, o.Phone
+                        FROM Owner o
+                        Left Join Neighborhood n on n.id=o.NeighborhoodId
+                        WHERE o.Id = @id
+                    ";
+
+                    cmd.Parameters.AddWithValue("@id", id);
+                    Owner owner= null;
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            if (owner == null)
+                            {
+                                owner = new Owner
+                                {
+                                    Id = reader.GetInt32(reader.GetOrdinal("ownerId")),
+                                    Email = reader.GetString(reader.GetOrdinal("Email")),
+                                    Name = reader.GetString(reader.GetOrdinal("Name")),
+                                    Address = reader.GetString(reader.GetOrdinal("Address")),
+                                    Neighborhood = new Neighborhood
+                                    {
+                                        Name = reader.GetString(reader.GetOrdinal("neighborhoodName"))
+                                    },
+                                    Phone = reader.GetString(reader.GetOrdinal("Phone")),
+                                };
+                            }
+                            }
+                       
+                        return owner;
+                    }
+                }
+            }
+        }
+    }
+}
